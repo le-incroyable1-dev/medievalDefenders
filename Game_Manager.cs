9a9c72b32bfbe8c6acafec631422b68d256e7fee
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2020-12-23 21:39:03 +0530

    Add files via upload
    The game manager script sets the game audio and takes care of what happens when the player beats the game or loses.

---
+++ Game_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public AudioSource game_audio;
+    public GameObject WinStruct;
+    public GameObject LoseStruct;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        game_audio = GetComponent<AudioSource>();
+        SetAudio();
+        //LoseStruct.SetActive(false);
+        //WinStruct.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+    
+    }
+
+    public void OnWin()
+    {
+        LoseStruct.SetActive(false);
+        WinStruct.SetActive(true);
+    }
+
+    public void OnLose()
+    {
+        WinStruct.SetActive(false);
+        LoseStruct.SetActive(true);
+    } 
+
+    public void SetAudio()
+    {
+        game_audio.volume = 1;
+    }
+
+
+}
