a4a41ca979e35a62e5203a856d3b2083a6c9506b
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2020-12-23 21:36:13 +0530

    Update ghost.cs
    Added functionality for the ghost and player reaction animations. Also allowed a special type of ghost to switch off the player spotlight when in contact.

--- ghost.cs
+++ ghost.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-
     public float speed = 1.0f;
+    public float WaitDur;
+    public GameObject player;
+    public Light player_Light;
+    public float dist;
+    public AudioClip player_Kill;
+
+    public string attackClipName;
 
     private Vector2 direction = Vector2.up;
     private float directionChangeTime = 0.0f; //the next time the ghost may change direction
 
     private Rigidbody2D rb;
     private CircleCollider2D cc;
+    private SpriteRenderer spr;
+    private Animator anim;
+    private Animator player_Anim;
+    private AudioSource player_Audio;
+
+    private bool isEnemy2 = false;
+
+
+    private void Awake()
+    {
+        if (gameObject.tag == "Enemy_2")
+            isEnemy2 = true;
+    }
 
     // Use this for initialization
     void Start()
     {
+        player_Anim = player.GetComponent<Animator>();
+        player_Audio = player.GetComponent<AudioSource>();
+
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         cc = GetComponent<CircleCollider2D>();
+        spr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -48,19 +72,19 @@ public class Ghost : MonoBehaviour
         rb.velocity = direction * speed;
         if (rb.velocity.x == 0)
         {
-            transform.position = new Vector2(Mathf.Round(transform.position.x), transform.position.y);
+            transform.position = new Vector2((transform.position.x), transform.position.y);
         }
         if (rb.velocity.y == 0)
         {
-            transform.position = new Vector2(transform.position.x, Mathf.Round(transform.position.y));
+            transform.position = new Vector2(transform.position.x, (transform.position.y));
         }
     }
     //for perpendilular up/down/left/right
     private bool canChangeDirection()
     {
-        Vector2 perpRight =extra.PerpendicularRight(direction);
+        Vector2 perpRight = Extra.PerpendicularRight(direction);
         bool openRight = openDirection(perpRight);
-        Vector2 perpLeft =extra.PerpendicularLeft(direction);
+        Vector2 perpLeft = Extra.PerpendicularLeft(direction);
         bool openLeft = openDirection(perpLeft);
         return openRight || openLeft;
     }
@@ -77,9 +101,9 @@ public class Ghost : MonoBehaviour
     private void changeDirection()
     {
         directionChangeTime = Time.time + 1; //can't change directions for within that second/or maybe in a sec not sure yet
-        Vector2 perRight = extra.PerpendicularRight(direction);
+        Vector2 perRight = Extra.PerpendicularRight(direction);
         bool openRight = openDirection(perRight);
-        Vector2 perLeft = extra.PerpendicularLeft(direction);
+        Vector2 perLeft = Extra.PerpendicularLeft(direction);
         bool openLeft = openDirection(perLeft);
         if (openRight || openLeft)
         {
@@ -87,10 +111,13 @@ public class Ghost : MonoBehaviour
             if (!openLeft || (choice == 0 && openRight))             //meeting points pe
             {
                 direction = perRight;
+                spr.flipX = true;
+
             }
             else
             {
                 direction = perLeft;
+                spr.flipX = false;
             }
         }
         else
@@ -101,30 +128,58 @@ public class Ghost : MonoBehaviour
     private bool openDirection(Vector2 dir)
     {
         RaycastHit2D[] rchObj = new RaycastHit2D[10];
-        float dist = 1;
+    
         cc.Cast(dir, rchObj, dist, true);
         foreach (RaycastHit2D rch in rchObj)
         {
-            if (rch && rch.collider.gameObject.tag == "Walls")
+            if (rch && rch.collider.gameObject.tag == "Wall")
             {
                 return false;
             }
         }
         return true;
     }
-    void OnCollisionEnter2D(Collision2D target)
+
+    
+    void OnTriggerEnter2D(Collider2D target)
     {
-        if (target.gameObject.tag == "Player")
+        if (target.gameObject.CompareTag("Player"))
         {
-            target.transform.position = new Vector2(0, 0);            //back to origin
+            //back to origin
+            StartCoroutine(OnPlayer());
         }
     }
-}
+    
+    private IEnumerator OnPlayer()
+    {
+        if (!Player.canKill)
+        {
+            anim.Play(attackClipName);
+            player_Anim.Play("player_Scared");
+            yield return new WaitForSecondsRealtime(WaitDur);
+            if(isEnemy2)
+            {
+                player_Light.intensity = 0.1f;
+            }
+            else
+                player.transform.position = new Vector2(0, 12);
+        }
+        else
+        {
+            player_Anim.Play("player_Attack");
+            player_Audio.clip = player_Kill;
+            player_Audio.Play();
+            yield return new WaitForSecondsRealtime(0.1f);
+            Destroy(gameObject);
+        }
 
+    }
+}
 
 /* =============================================================================
 #  Author:          Divya Gandhi - https://github.com/divya16-bit
 #  Email:           16gandhi.hemani@gmail.com
 #  FileName:        ghost.cs
 #  Created On:      26/11/2020
+#  Updated On :     23/12/2020 by Aurav
 ============================================================================= */
