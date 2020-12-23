8305f83b1b04081a620e9cd4c172921a97329164
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2020-12-23 21:33:07 +0530

    Update Player.cs
    Added changes for the score and its display. Also included another feature that allows the player to switch the spotlight on again if it was turned off by a ghost attack.

--- Player.cs
+++ Player.cs
@@ -1,104 +1,231 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     private float speed = 10.0f;
-    private Animator anim;
-    private Transform faceTransform;
     private float step;
-    private Vector2 target_pos;
+    private float counter = 10.0f;
 
+    private CircleCollider2D cc;
+    //private Animator anim;
+    private SpriteRenderer spr;
+    private AudioSource src; 
+
+    private bool UpPress = false;
+    private bool DownPress = false;
+    private bool RightPress = false;
+    private bool LeftPress = false;
+
+    public Game_Manager game_Manager;
+
+    public static bool canKill = false;
 
     public float MaxRayDist;
     public float MoveDist;
-    //public GameObject playerFace;
-    public Text score_Text;
     public float score;
 
+    public AudioClip pickupClip;
+    public AudioClip musicGen;
+    public AudioClip musicActiv;
+    //public GameObject playerFace;
+    public GameObject counterShow;
+    public Light player_Light;
+    public AudioSource intense_src;
+
+    public TextMeshProUGUI score_Text;
+    public TextMeshProUGUI counter_Text;
+
+    public Vector2 target_pos;
+
+
+    //TEST
+    //public bool test_CanMove = true;
 
     // Start is called before the first frame update
-    // Sets up some basic parameters for the player
     void Start()
     { 
-        anim = GetComponent<Animator>();
-        //faceTransform = playerFace.GetComponent<Transform>();
+        //anim = GetComponent<Animator>();
+        cc = GetComponent<CircleCollider2D>();
+        spr = GetComponent<SpriteRenderer>();
+        src = GetComponent<AudioSource>();
+        
+        intense_src.volume = 0;
+
         score = 0;
         score_Text.text = score.ToString();
         target_pos = transform.position;
     }
-    
-    // Ensures the player never forgets to move when it is told to
+
     private void Update()
     {
-        
-        step = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, target_pos, step);
-    }
-    
-    // Checks whether the player can move in a direction 'dir'
-    bool CheckMove(Vector2 dir)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, MaxRayDist);
 
-        if (hit.collider == null)
-            return true;
-        else
-            return false;
         
+        if (UpPress)
+        {
+            if (CheckMove(Vector2.up))
+            {
+                target_pos = new Vector2(transform.position.x, transform.position.y + MoveDist);
+                step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, target_pos, step);
+            }
+        }
+        else if (DownPress)
+        {
+            if (CheckMove(Vector2.down))
+            {
+
+                target_pos = new Vector2(transform.position.x, transform.position.y - MoveDist);
+                step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, target_pos, step);
+            }
+        }
+        else if (RightPress)
+        {
+            if (CheckMove(Vector2.right))
+            {
+                target_pos = new Vector2(transform.position.x + MoveDist, transform.position.y);
+                step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, target_pos, step);
+                spr.flipX = false;
+            }
+        }
+        else if (LeftPress)
+        {
+            if (CheckMove(Vector2.left))
+            {
+                target_pos = new Vector2(transform.position.x - MoveDist, transform.position.y);
+                step = speed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, target_pos, step);
+                spr.flipX = true;
+
+            }
+        }
+
+        //
+       if(canKill)
+        {
+            if(((int)counter) > 0)
+            {
+                counter_Text.text = ((int)counter).ToString();
+                counter -= Time.deltaTime;
+            }
+            else
+            {
+                counterShow.SetActive(false);
+                src.clip = pickupClip;
+                intense_src.volume = 0;
+                game_Manager.SetAudio();
+                counter = 10.0f;
+                canKill = false;
+            }
+        }
+
+
     }
-    
-    // Detects pickups in the game and increments the score
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private bool CheckMove(Vector2 dir)
     {
-        if(collision.gameObject.CompareTag("Pickup"))
+        RaycastHit2D[] rchObj = new RaycastHit2D[10];
+
+        cc.Cast(dir, rchObj, MaxRayDist, true);
+        foreach (RaycastHit2D rch in rchObj)
+        {
+            if (rch && rch.collider.gameObject.tag == "Wall")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("kp") && (!canKill))
+        {
+            collision.gameObject.SetActive(false);
+            canKill = true;
+            intense_src.volume = 1;
+            player_Light.intensity = 1.1f;
+            game_Manager.SetAudio();
+            counterShow.SetActive(true);
+              
+        }
+        else if (collision.gameObject.CompareTag("Pickup"))
         {
             score += 1;
             score_Text.text = score.ToString();
+            src.Play();
+            collision.gameObject.SetActive(false);
         }
+
     }
-    
-    // These public functions can be used through UI Buttons inside the Unity engine
+
+    //
     public void MoveUp()
     {
+        UpPress = true;
         
-        if (CheckMove(Vector2.up))
-        {
-           target_pos = new Vector2(transform.position.x, transform.position.y + MoveDist);
-        }
     }
     public void MoveDown()
     {
+        DownPress = true;
         
-        if (CheckMove(Vector2.down))
-        {
-
-            target_pos = new Vector2(transform.position.x, transform.position.y - MoveDist);
-        }
     }
     public void MoveRight()
     {
-        if (CheckMove(Vector2.right))
-        {
-            target_pos = new Vector2(transform.position.x + MoveDist, transform.position.y);
-        }
+        RightPress = true;
     }
     public void MoveLeft()
     {
+        LeftPress = true;
         
-        if (CheckMove(Vector2.left))
-        {
-            target_pos = new Vector2(transform.position.x - MoveDist, transform.position.y);
-        }
     }
+    public void MoveUpRelease()
+    {
+        UpPress = false;
+
+    }
+    public void MoveDownRelease()
+    {
+        DownPress = false;
+    }
+    public void MoveRightRelease()
+    {
+        RightPress = false;
+    }
+    public void MoveLeftRelease()
+    {
+        LeftPress = false;
+    }
+    //
+
+    //TEST
+    /*bool CheckMove(Vector2 dir)
+   {
+       RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, MaxRayDist);
+
+       if (hit.collider == null || hit.collider.gameObject.tag == "Player")
+           return true;
+       else
+           return false;
+
+   }*/
+
+
 }
+
+
 
 /* =============================================================================
 #  Author:          Aurav S Tomar - https://github.com/le-incroyable1-dev
 #  Email:           aurav.tomar@gmail.com
 #  FileName:        Player.cs
+#  Updated On:      23/12/2020
 #  Created On:      24/11/2020
 ============================================================================= */
 
