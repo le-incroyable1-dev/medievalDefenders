ef86c6f98b244b0b9bf7fcafec133be73b302fd3
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-03-05 08:10:02 +0530

    Add files via upload
    Linearly teleports the player from an entry point to an exit point

---
+++ teleporter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporter : MonoBehaviour
+{
+    public Transform tPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Transform collisionTransform = collision.gameObject.GetComponent<Transform>();
+
+        if (collision.gameObject.CompareTag("Player"))
+            collisionTransform.position = Vector3.Lerp(collisionTransform.position, tPoint.position, 1.5f);
+    }
+}
