e3b5fd6d740376de9b443e97cf005e63baf03f4b
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-05-13 01:10:12 +0530

    Update Game_Manager.cs

--- Game_Manager.cs
+++ Game_Manager.cs
@@ -1,42 +1,149 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Game_Manager : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.Pacman
 {
-    public AudioSource game_audio;
-    public GameObject WinStruct;
-    public GameObject LoseStruct;
-
-
-    // Use this for initialization
-    void Start()
+    public class Game_Manager : MonoBehaviourPunCallbacks
     {
-        game_audio = GetComponent<AudioSource>();
-        SetAudio();
-        //LoseStruct.SetActive(false);
-        //WinStruct.SetActive(false);
-        DontDestroyOnLoad(gameObject);
-    
+
+        #region Public Fields
+
+        public AudioSource game_audio;
+        public GameObject WinStruct;
+        public GameObject LoseStruct;
+        public static Game_Manager Instance;
+
+        [Tooltip("This is the player prefab that will be spawned.")]
+        public GameObject playerPrefab;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+
+
+        #endregion
+
+        #region Photon CallBacks
+
+
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public override void OnPlayerEnteredRoom(Photon.Realtime.Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Photon.Realtime.Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+                LoadArena();
+            }
+        }
+
+        #endregion
+
+
+        #region Public Methods
+
+
+        public void LeaveRoom()
+        {
+            //Destroy(GameObject.Find("Canvas"));
+            //Destroy(PlayerManager.LocalPlayerInstance.GetComponent<PlayerManager>().PlayerUiPrefab);
+            PhotonNetwork.LeaveRoom();
+        }
+
+        public void OnWin()
+        {
+            LoseStruct.SetActive(false);
+            WinStruct.SetActive(true);
+        }
+
+        public void OnLose()
+        {
+            WinStruct.SetActive(false);
+            LoseStruct.SetActive(true);
+        }
+
+        public void SetAudio()
+        {
+            game_audio.volume = 1;
+        }
+
+
+        #endregion
+
+
+        #region Private Methods
+
+        private void Start()
+        {
+            game_audio = GetComponent<AudioSource>();
+            SetAudio();
+            //LoseStruct.SetActive(false);
+            //WinStruct.SetActive(false);
+            //DontDestroyOnLoad(gameObject);
+
+            Instance = this;
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                if (Player.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 12f, 0f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
+
+            }
+
+        }
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for 1");
+        }
+
+        #endregion
     }
-
-    public void OnWin()
-    {
-        LoseStruct.SetActive(false);
-        WinStruct.SetActive(true);
-    }
-
-    public void OnLose()
-    {
-        WinStruct.SetActive(false);
-        LoseStruct.SetActive(true);
-    } 
-
-    public void SetAudio()
-    {
-        game_audio.volume = 1;
-    }
-
-
 }
 
 /* =============================================================================
@@ -44,4 +151,5 @@ public class Game_Manager : MonoBehaviour
 #  Email:           aurav.tomar@gmail.com
 #  FileName:        Player.cs
 #  Created On:      26/11/2020
+#  Updated On:      13/05/2021
 ============================================================================= */
