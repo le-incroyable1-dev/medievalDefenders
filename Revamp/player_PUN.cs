ce781f5f6d85458788988e9534a6c5930bd49838
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-07-15 14:02:52 +0530

    Update player_PUN.cs

--- Revamp/player_PUN.cs
+++ Revamp/player_PUN.cs
@@ -69,11 +69,14 @@ void OnLevelWasLoaded(int level)
 
         void CalledOnLevelWasLoaded(int level)
         {
+            /*
             // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
             if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
             {
                 transform.position = new Vector3(0f, 5f, 0f);
             }
+            */
+            //Not required as of now
         }
 
 
